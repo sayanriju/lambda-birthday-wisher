13bd394bf1e9d286b5704ef723f3146bea6e7472
Sayan Riju Chakrabarti <s26c.sayan@gmail.com>
2018-05-04 13:13:11 +0530

    Fri May  4 13:13:11 IST 2018

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const { isSameDay, addMinutes, setYear, getYear } = require("date-fns")
+const {
+  isSameDay, addMinutes, setYear, getYear
+} = require("date-fns")
 const ses = require("node-ses")
 const client = ses.createClient()
 
@@ -10,29 +12,29 @@ const sendEmail = person => new Promise(resolve => client.sendEmail({
 }, (err) => {
   if (err) {
     // console.log("[[SES ERR]] ", err)
-    resolve("[[SES ERR]] " + person.email)
+    resolve(`[[SES ERR]] ${person.email}`)
   } else {
     // console.log("[[SES SUCCESS]] " + person.email);
-    resolve("[[SES SUCCESS]] " + person.email);
+    resolve(`[[SES SUCCESS]] ${person.email}`)
   }
 }))
 
 exports.handler = function (event, context) {
-  const now = event.now || new Date()  // in UTC
+  const now = event.now || new Date() // in UTC
   const offset = event.utc_offset_minutes
-  const people = event.people
+  const { people } = event.people
   const promises = []
-  console.log("==> %s, %s", offset, JSON.stringify(people));
+  console.log("==> %s, %s", offset, JSON.stringify(people))
   people
-  .map(p => ({
-    name: p.name,
-    email: p.email,
-    birthday: addMinutes(p.birthday, offset)  // to UTC
-  }))
-  .filter(p => isSameDay(setYear(p.birthday, getYear(now)), now))
-  .forEach(p => promises.push(sendEmail(p)))
+    .map(p => ({
+      name: p.name,
+      email: p.email,
+      birthday: addMinutes(p.birthday, offset) // to UTC
+    }))
+    .filter(p => isSameDay(setYear(p.birthday, getYear(now)), now))
+    .forEach(p => promises.push(sendEmail(p)))
 
   Promise.all(promises)
-  .then(() => context.succeed("OK"))
-  .catch((e) => context.fail(e.message))
+    .then(() => context.succeed("OK"))
+    .catch(e => context.fail(e.message))
 }
